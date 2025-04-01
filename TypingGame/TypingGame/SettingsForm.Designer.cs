80ee08862edd9ee714e11efc93fd230451f15a3b
riklapp <riklappfeed@gmail.com>
2025-04-01 19:30:06 +0500

    Update SettingsForm.Designer.cs

--- TypingGame/TypingGame/SettingsForm.Designer.cs
+++ TypingGame/TypingGame/SettingsForm.Designer.cs
@@ -27,22 +27,18 @@
             this.chkFullscreen = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
 
-            // TableLayoutPanel settings
             this.tableLayoutPanel.ColumnCount = 2;
             this.tableLayoutPanel.RowCount = 3;
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
 
-            // ComboBox for Resolution settings
             this.cmbResolution.Dock = System.Windows.Forms.DockStyle.Fill;
             this.cmbResolution.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 
-            // CheckBox for Fullscreen settings
             this.chkFullscreen.Dock = System.Windows.Forms.DockStyle.Fill;
             this.chkFullscreen.Text = "Fullscreen";
 
-            // Button settings
             this.btnApply.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnApply.Text = "Apply";
             this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
@@ -51,16 +47,13 @@
             this.btnCancel.Text = "Cancel";
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
 
-            // Add controls to the TableLayoutPanel
             this.tableLayoutPanel.Controls.Add(this.cmbResolution, 0, 0);
             this.tableLayoutPanel.Controls.Add(this.chkFullscreen, 0, 1);
             this.tableLayoutPanel.Controls.Add(this.btnApply, 0, 2);
             this.tableLayoutPanel.Controls.Add(this.btnCancel, 1, 2);
 
-            // Add TableLayoutPanel to the Form
             this.Controls.Add(this.tableLayoutPanel);
 
-            // Other form settings
             this.Name = "SettingsForm";
             this.Text = "Settings";
             this.Load += new System.EventHandler(this.SettingsForm_Load);
