16adb58bd0361564450a54409c098c78ef6290e3
riklapp <riklappfeed@gmail.com>
2025-04-01 19:29:41 +0500

    Update SettingsForm.cs

--- TypingGame/TypingGame/SettingsForm.cs
+++ TypingGame/TypingGame/SettingsForm.cs
@@ -13,29 +13,24 @@ namespace RPGGame
 
         private void SettingsForm_Load(object sender, EventArgs e)
         {
-            // Add common screen resolutions to the ComboBox
             cmbResolution.Items.Add("800x600");
             cmbResolution.Items.Add("1024x768");
             cmbResolution.Items.Add("1280x720");
             cmbResolution.Items.Add("1920x1080");
-            cmbResolution.SelectedIndex = 3; // Default to 1920x1080
+            cmbResolution.SelectedIndex = 3;
 
-            // Set default fullscreen mode
             chkFullscreen.Checked = true;
         }
 
         private void btnApply_Click(object sender, EventArgs e)
         {
-            // Parse the selected resolution
             string selectedResolution = cmbResolution.SelectedItem.ToString();
             string[] dimensions = selectedResolution.Split('x');
             int width = int.Parse(dimensions[0]);
             int height = int.Parse(dimensions[1]);
-
-            // Get the fullscreen mode
+e
             bool isFullscreen = chkFullscreen.Checked;
 
-            // Apply the resolution and fullscreen mode to the main form
             MainForm mainForm = (MainForm)this.Owner;
             mainForm.ChangeResolution(width, height, isFullscreen);
 
@@ -44,7 +39,7 @@ namespace RPGGame
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
-            this.Close(); // Close the settings form
+            this.Close();
         }
     }
 }
